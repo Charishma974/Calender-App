bd1cfc50143170d7504bcf07f36d9c086e4cfcbe
Charishma974 <cherryspl09@gmail.com>
2024-09-05 10:45:46 +0530

    Grey Scientific Labs Assignment

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CalenderApp from './components/CalenderApp.jsx'
+import "./components/CalenderApp.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalenderApp />
+    </div>
+  )
+}
+
+export default App
